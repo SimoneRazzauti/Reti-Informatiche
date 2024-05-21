212586673fb02bb2449d4df6e255c35550a662e6
SimoneRazzauti <s.razzauti97@gmail.com>
2024-05-21 17:22:55 +0200

    ok

--- Razzauti/codice/client.c
+++ Razzauti/codice/client.c
@@ -1,13 +1,77 @@
-int main(){
-    int sockfd, ret;
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <sys/time.h>
+#include <time.h>
+
+#define LEN_REPLY 6 // lunghezza del messaggio di risposta
+#define BUFFER_SIZE 1024
+#define WELCOME_CLIENT "\n*********************** BENVENUTO CLIENTE ************************\n*               Comandi disponibili!                   *\n*                                                        *\n* find --> ricerca la disponibilità per una prenotazione *\n* book --> invia una prenotazione                        *\n* esc --> termina il client                              *\n*                                                        *\n**********************************************************\n"
+int main(int argc, char *argv[]){
+    int sockfd, ret; // variabili utili al socket
+    char buffer[BUFFER_SIZE];
+    struct  sockaddr_in server_addr, client_addr;
+    in_port_t door = htons(atoi(argv[1])); // utilizzo della funzione atoi per convertire la stringa rappresentante il numero di porta inserito dall'utente da terminale in un intero
+
+    // Utility
 
     // Creazione del socket tramite funzione SOCKET
+    memset((void*)&client_addr, 0, sizeof(client_addr)); // pulizia
+    memset((void*)&server_addr, 0, sizeof(server_addr)); // pulizia
+
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
     if(sockfd < 0){
         perror("ERRORE: Impossibile creare un nuovo socket");
         exit(1);
     }
 
+    // Inizializzazione della struttura client
+    client_addr.sin_family = AF_INET;
+    client_addr.sin_port = door;
+    client_addr.sin_addr.s_addr = INADDR_ANY;
 
+    ret = bind(sockfd, (struct sockaddr*)&client_addr, sizeof(client_addr));
+    if(ret < 0){
+        perror("ERRORE: errore nella bind del client\n");
+        printf("ARRESTO DEL SISTEMA IN CORSO...\n");
+        ffush(stdout);
+        exit(1);
+    }
+
+    // Inizializzazione della struttura server
+    server_addr.sin_family = AF_INET;
+    server_addr.port = door;
+    server_addr.sin_addr.s_addr = INADDR_ANY;
+
+    ret = connnect(sockfd, (struct sockaddr*)&server_addr, sizeof(server_addr));
+    if(ret < 0){
+        perror("ERRORE: errore nella connect del client\n");
+        printf("ARRESTO DEL SISTEMA IN CORSO...\n");
+        ffush(stdout);
+        exit(1);
+    }
+    len = LEN_REPLY;
+    ret = recv(sockfd, (void*)buffer, len, 0);
+    if(ret < 0){
+        perror("ERRORE: errore nella ricezione\n");
+        fflush(stdout);
+        close(sockfd);
+        exit(1);
+    }
+
+    // Aggiungo il terminatore stringa che non viene inviato
+    buffer[len] = '\0';
+
+    printf("%s\n", buffer);
+    fflush(stdout);
+    printf(WELCOME_CLIENT);
+    fflush(stdout);
+
+    close(sockfd);
     return 0;
 }
--- Razzauti/codice/server.c
+++ Razzauti/codice/server.c
@@ -15,9 +15,12 @@
 #define BUFFER_SIZE 1024
 #define WELCOME_SERVER "AVVIO DEL SERVER\nCOMANDI DISPONIBILI:\n1) stat ->  restituisce le comande del giorno per tavolo o per status ('a', 'p' o 's') \n2) stop ->  il server si arresta (solo se non ci sono comande in attesa o in preparazione\n"
 
-int main(){
-    int sockfd;
-    struct sockaddr_in server_addr, client_addr;
+int main(int argc, char* argv[]){
+    int sockfd, new_sockfd;
+    struct sockaddr_in server_addr, client_addr; // per le primitive bind e accept
+    in_port_t door = htons(atoi(argv[1])); // uso la funzione atoi che fa parte della libreria stdlib.h per convertire una stringa in un intero, in questo modo converto la stringa passata dall'utente in un intero che rappresenta la porta del server 
+    char buffer[BUFFER_SIZE];
+    int ret, len;
 
     // Creazione del socket tramite funzione SOCKET
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
@@ -26,9 +29,13 @@ int main(){
         exit(1);
     }  
 
-    // Inizializzazione array e tabella 
+    // Inizializzazione array e tabella per tavoli e comande
 
     // Inizializzazione della struttura per l'indirizzo del server
+    memset(&server_addr, 0, sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = INADDR_ANY;
+    server_addr.sinport = door;
 
     // BIND sul nuovo socket creato
     if(bind(sockfd, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0){
@@ -50,6 +57,31 @@ int main(){
 
     // Ciclo principale
     while(1){
+        // Pronto descrittore socket di ascolto
 
+        // Dimensione dell'indirizzo del client
+        socklen_t client_len = sizeof(client_addr)
+
+        // Accetto nuove connesioni
+        new_sockfd = accept(sockfd, (struct sockaddr*)&client_addr, sizeof(client_addr));
+        if(new_sockfd < 0){
+            perror("ERRORE: Impossibile creare un nuovo socket di ascolto\n");
+            exit(1);
+        }
+
+        // Creo la risposta
+        strcpy(buffer,"HELLO!");
+        len = strlen(buffer);
+
+        // Invio risposta senza \0
+        ret = send(new_sockfd, (void*)buffer, len, 0);
+
+        if(ret < 0){
+            perror("Errore nella fase di invio della send\n");
+            exit(1);
+        }
+        close(new_sockfd);
     }
+    close(sockfd);
+    return 0;
 }

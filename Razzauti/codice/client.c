257b7aba2043be05792420133d109c4ff1a412bf
SimoneRazzauti <s.razzauti97@gmail.com>
2024-05-21 18:33:28 +0200

    ip

--- Razzauti/codice/client.c
+++ Razzauti/codice/client.c
@@ -10,18 +10,30 @@
 #include <time.h>
 
 #define PORTA 4242 // porta del server in ascolto
-#define LEN_REPLY 6 // lunghezza del messaggio di risposta
-#define BUFFER_SIZE 1024 // dimensione massima del buffer
+#define BUFFER_SIZE 1024 // dimensione massima del buffer in byte
+
+#define MAX_WORDS 10       // Numero massimo di parole che possono essere estratte dalla frase
+#define MAX_WORD_LENGTH 50 // Lunghezza massima di ogni parola
+
+#define GIORNI_IN_UN_MESE 31 // informazioni che mi serviranno nella funziona di verifica Data corretta
+#define GIORNI_FEBBRAIO 28
+#define GIORNI_FEBBRAIO_BISESTILE 29
+#define GIORNI_IN_UN_MESE_CORTO 30
+
+#define validLen 2  // Lunghezza codici fissati tra client - server per send/ recv
+#define codiceLen 5 // lunghezza dei codici da mandare al server
 
 #define WELCOME_CLIENT "\n*********************** BENVENUTO CLIENTE ************************\n*               Comandi disponibili!                   *\n*                                                        *\n* find --> ricerca la disponibilità per una prenotazione *\n* book --> invia una prenotazione                        *\n* esc --> termina il client                              *\n*                                                        *\n**********************************************************\n"
 
+// FUNZIONI DI UTILITA' (avrei potuto scriverle in un file separato ma avrei dovuto riscrivere il makefile, ho lasciato in un file per compattezza)
+
+
+
 int main(int argc, char *argv[]){
     int sockfd, ret; // variabili utili al socket
-    int len;
     char buffer[BUFFER_SIZE];
     struct  sockaddr_in server_addr, client_addr;
-    in_port_t door = htons(atoi(argv[1])); // utilizzo della funzione atoi per convertire la stringa rappresentante il numero di porta inserito dall'utente da terminale in un intero
-
+    in_port_t door = htons(atoi(argv[1])); // funzione atoi: converte la stringa in un intero che rappresenta il numero di porta inserito dall'utente da terminale gnome
     // Utility
 
     // Creazione del socket tramite funzione SOCKET
@@ -30,7 +42,7 @@ int main(int argc, char *argv[]){
 
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
     if(sockfd < 0){
-        perror("ERRORE: Impossibile creare un nuovo socket");
+        perror("ERRORE: Impossibile creare un nuovo socket\n");
         exit(1);
     }
 

0ef99278960e4429bc00b7d2e169efc598cedc89
SimoneRazzauti <s.razzauti97@gmail.com>
2024-05-24 11:23:10 +0200

    up

--- Falleni/cli.c
+++ Falleni/cli.c
@@ -9,10 +9,8 @@
 #include <sys/time.h>
 #include <time.h>
 
-#define validLen 2 
-#define PORTA 4242 // porta del server in ascolto
-#define BUFFER_SIZE 1024 // dimensione massima del buffer in byte
-#define WELCOME_CLIENT "\n*********************** BENVENUTO CLIENTE ************************\n*Comandi disponibili!*\n**\n* find --> ricerca la disponibilità per una prenotazione*\n* book --> invia una prenotazione*\n*esc --> termina il client*\n**\n**********************************************************\n"
+#define PORT 4242        // porta del server
+#define BUFFER_SIZE 1024 // dimensione massima del buffer
 
 #define MAX_WORDS 10       // Numero massimo di parole che possono essere estratte dalla frase
 #define MAX_WORD_LENGTH 50 // Lunghezza massima di ogni parola
@@ -21,44 +19,117 @@
 #define GIORNI_FEBBRAIO 28
 #define GIORNI_FEBBRAIO_BISESTILE 29
 #define GIORNI_IN_UN_MESE_CORTO 30
-/* ########### FINE FUNZIONI DI UTILITA' ###################### */
 
+#define validLen 2  // Lunghezza codici fissati tra client - server per send/ recv
+#define codiceLen 5 // lunghezza dei codici da mandare al server
 
-// Invia al socket in input il messaggio dentro buffer
-int invia(int j, char* buffer) {
-	int ret;
-	// Comunico l'esito
-	return ret;
+void comandi_cliente()
+{
+    printf("\n*********************** BENVENUTO CLIENTE ************************\n");
+    printf("*               Comandi per prenotare!                   *\n");
+    printf("*                                                        *\n");
+    printf("* find --> ricerca la disponibilità per una prenotazione *\n");
+    printf("* book --> invia una prenotazione                        *\n");
+    printf("* esc --> termina il client                              *\n");
+    printf("*                                                        *\n");
+    printf("**********************************************************\n");
+    fflush(stdout);
 }
 
-// Ricevi dal socket in input la lunghezza del messaggio e lo mette dentro lmsg
-int riceviLunghezza(int j, int *lmsg) {
-	int ret;
-	ret = recv(j, (void*)lmsg, sizeof(uint16_t), 0);
-	return ret;
+// Verifica se la data e l'ora passate come parametro sono valide
+int data_valida(int GG, int MM, int AA, int HH)
+{
+    int is_valid = 0;
+    if (MM < 1 || MM > 12)
+    {
+        return 0; // mese non valido
+    }
+    // Verifica se l'anno è bisestile
+    if (AA % 4 == 0 && (AA % 100 != 0 || AA % 400 == 0))
+    {
+        switch (MM)
+        { // verifica mesi
+        case 2:
+            is_valid = (GG >= 1 && GG <= GIORNI_FEBBRAIO_BISESTILE); // verifica il mese di febbraio bisestile
+            break;
+        case 4:
+        case 6:
+        case 9:
+        case 11:
+            is_valid = (GG >= 1 && GG <= GIORNI_IN_UN_MESE_CORTO); // verifica i mesi con 30 giorni
+            break;
+        default:
+            is_valid = (GG >= 1 && GG <= GIORNI_IN_UN_MESE); // verifica i mesi con 31 giorni
+            break;
+        }
+    }
+    else
+    { // anno non bisestile
+        switch (MM)
+        { // verifica mesi
+        case 2:
+            is_valid = (GG >= 1 && GG <= GIORNI_FEBBRAIO); // verifica il mese di febbraio non bisestile
+            break;
+        case 4:
+        case 6:
+        case 9:
+        case 11:
+            is_valid = (GG >= 1 && GG <= GIORNI_IN_UN_MESE_CORTO); // verifica i mesi con 30 giorni
+            break;
+        default:
+            is_valid = (GG >= 1 && GG <= GIORNI_IN_UN_MESE); // verifica i mesi con 31 giorni
+            break;
+        }
+    }
+    return is_valid && HH >= 10 && HH <= 23; // verifica ora. Orario valido dalle 10 alle 23.
+}
+int data_futura(int GG, int MM, int AA, int HH)
+{
+    // Inizializzo a zero una variabile time_t per contenere il tempo corrente
+    time_t input_time = 0;
+    // Ottengo la struttura tm corrispondente al tempo corrente
+    struct tm *input_timeinfo = localtime(&input_time);
+    // Imposto i campi della struttura tm con i valori passati come parametro
+    input_timeinfo->tm_mday = GG;
+    input_timeinfo->tm_mon = MM - 1;
+    // Converto l'anno in formato a quattro cifre, se necessario
+    input_timeinfo->tm_year = (AA < 100 ? AA + 2000 - 1900 : AA - 1900);
+    input_timeinfo->tm_hour = HH;
+    // Converto la struttura tm in un tempo in formato time_t
+    input_time = mktime(input_timeinfo);
+    // Confronto il tempo corrente con il tempo della data passata come parametro
+    // e restituisco 1 se la data è futura, 0 altrimenti
+    return (input_time >= time(NULL));
 }
 
-// Riceve dal socket in input il messaggio e lo mette dentro buffer
-int ricevi(int j, int lunghezza, char* buffer) {
-	int ret;
-	ret = recv(j, (void*)buffer, lunghezza, 0);
-	return ret;
+void gestione_errore(int ret, int socket)
+{ // Gestione errori e gestione in caso di chiusura del socket remoto, termino.
+    if (ret == -1)
+    {
+        perror("ERRORE nella comunicazione con il server\n");
+        printf("ARRESTO IN CORSO...\n\n");
+        fflush(stdout);
+        close(socket);
+        exit(1);
+    }
+    else if (ret == 0)
+    {
+        printf("AVVISO: il server ha chiuso il socket remoto.\nARRESTO IN CORSO...\n");
+        fflush(stdout);
+        close(socket);
+        exit(1);
+    }
 }
 
-
-int main(int argc, char *argv[]){
-    printf("ciao uno");
-    // Variabili dei socket
-    int sockfd, ret, i, lmsg;
-    struct  sockaddr_in server_addr, client_addr;
+int main(int argc, char *argv[])
+{
+    int sockfd, ret, max_fd; // variabili per i socket
     char buffer[BUFFER_SIZE];
-    
-    // Variabili di utilità per l'IO-MULTIPLEXING
-    fd_set master; // set di descrittori da monitorare
-    fd_set read_fds; // set di descrittori in stato pronti
-    int fdmax; // descrittore max
+    struct sockaddr_in serv_addr, cli_addr;
+    in_port_t porta = htons(atoi(argv[1])); // utilizzo della funzione atoi per convertire la stringa rappresentante il numero di porta inserito dall'utente da terminale in un intero
 
-    // Varibili per le funzioni find e book
+    // variabili di utilità
+    char identificativo[] = "C\0"; // codice per riconoscere il Client
     char *codice = NULL;           // find o book
     char cognome[30];
     int quantita, wordLen, numPersone, tavoliDisp = 0; // tavoliDisp = numero tavoli restituiti dalla Find
@@ -68,25 +139,46 @@ int main(int argc, char *argv[]){
     char *datiInformazioni[MAX_WORDS]; // L'array di puntatori in cui vengono memorizzate le parole
     int word_count = 0;                // Il numero di parole estratte dalla frase
 
-    // Creazione del socket tramite funzione SOCKET
+    fd_set master; // variabili per set
+    fd_set read_fds;
+
+    uint32_t len_HO; // lunghezza del messaggio espressa in host order
+    uint32_t len_NO; // lunghezza del messaggio espressa in network order
+
+    // CREAZIONE SOCKET
+    memset((void *)&cli_addr, 0, sizeof(cli_addr));
+    memset((void *)&serv_addr, 0, sizeof(serv_addr));
+
+    // Creazione del socket
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if(sockfd < 0){
-        perror("ERRORE: Impossibile creare un nuovo socket\n");
+    if (sockfd < 0)
+    {
+        perror("Errore nella creazione del socket");
+        exit(1);
+    }
+
+    // Inizializzazione della struttura
+    cli_addr.sin_family = AF_INET;
+    cli_addr.sin_port = porta;
+    cli_addr.sin_addr.s_addr = INADDR_ANY;
+    ret = bind(sockfd, (struct sockaddr *)&cli_addr, sizeof(cli_addr));
+    if (ret == -1)
+    {
+        perror("ERRORE nella bind()");
+        printf("ARRESTO IN CORSO...\n");
         fflush(stdout);
         exit(1);
     }
-printf("ciao 2");
-    // Inizializzazione della struttura dell indirizzo del server
-    memset((void*)&server_addr, 0, sizeof(server_addr)); // pulizia
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_port = htons(PORTA);
-    inet_pton(AF_INET, "127.0.0.1", &server_addr.sin_addr); // localhost
-printf("ciao 3");
-    // Connessione tramite la primitiva CONNECT
-    ret = connect(sockfd, (struct sockaddr*)&server_addr, sizeof(server_addr));
-    if(ret < 0){
-        perror("ERRORE: errore nella connect del client\n");
-        printf("ARRESTO DEL SISTEMA IN CORSO...\n");
+
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(PORT);
+    serv_addr.sin_addr.s_addr = INADDR_ANY;
+
+    ret = connect(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr));
+    if (ret == -1)
+    {
+        perror("ERRORE nella connect()");
+        printf("ARRESTO IN CORSO...\n");
         fflush(stdout);
         exit(1);
     }
@@ -97,20 +189,224 @@ printf("ciao 3");
     FD_SET(0, &master);
     FD_SET(sockfd, &master);
 
-    fdmax = sockfd;
-
-printf("ciao dopo la conenct");
-    // Invio del codice identificativo al server: client == 'C'
-    
-    // Invio del codice identificativo al server: client == 'C'
-    strcpy(buffer, "C\0");
-    char identificativo[] = "C\0";
-    int len = strlen(buffer)+1;
-
+    max_fd = sockfd;
+    // invio codice identificativo Client
     ret = send(sockfd, (void *)identificativo, validLen, 0);
+    gestione_errore(ret, sockfd);
 
+    ret = recv(sockfd, (void *)buffer, validLen, 0);
+    gestione_errore(ret, sockfd);
+    if (buffer[0] != 'S')
+    {
+        perror("Troppi Client connessi. RIPROVARE.\n\n");
+        fflush(stdout);
+        close(sockfd);
+        exit(1);
+    }
 
-    printf("OK");
+    comandi_cliente();
+    while (1)
+    {
+        memset(buffer, 0, sizeof(buffer)); // In caso contrario, rimarrebbe l'ultima cosa che c'era dentro.
+        read_fds = master;
+        select(max_fd + 1, &read_fds, NULL, NULL, NULL);
 
-    printf("esco");
-}//ciao
+        if (FD_ISSET(sockfd, &read_fds))
+        { // PRONTO SOCKET DI COMUNICAZIONE
+            ret = recv(sockfd, &len_NO, sizeof(uint32_t), 0);
+            gestione_errore(ret, sockfd);
+
+            len_HO = ntohl(len_NO);
+            ret = recv(sockfd, buffer, len_HO, 0);
+            if (strncmp(buffer, "STOP", strlen("STOP")) == 0)
+            { // se il server ha usato lo "stop", avviso e termino.
+                printf("AVVISO: il server è stato arrestato.\n\n");
+                fflush(stdout);
+                close(sockfd);
+                exit(0);
+            }                       // altrimenti, il server sta inviando un messaggio da stampare
+            printf("%s\n", buffer); // Mi serve allora solo stampare il messaggio.
+            fflush(stdout);
+        }
+        else // pronto il socket di input
+        {
+            
+            fgets(buffer, BUFFER_SIZE, stdin);
+
+            // Estrae le parole dalla frase utilizzando la funzione 'strtok'
+            char *word = strtok(buffer, " "); // Estrae la prima parola utilizzando lo spazio come delimitatore
+            word_count = 0;
+            while (word != NULL && word_count < MAX_WORDS)
+            { // Finchè ci sono parole da estrarre e non si supera il limite massimo
+                // Rimuove il carattere di fine riga dalla parola se presente
+                wordLen = strlen(word);
+                if (word[wordLen - 1] == '\n')
+                {
+                    word[wordLen - 1] = '\0';
+                }
+
+                // Aggiunge la parola all'array di parole
+                datiInformazioni[word_count] = word; // Memorizza il puntatore alla parola nell'array
+                word_count++;                        // Incrementa il contatore di parole estratte
+
+                // Estrae la prossima parola
+                word = strtok(NULL, " "); // Utilizza 'NULL' come primo parametro per estrarre le parole successive
+            }
+
+            if (strcmp(datiInformazioni[0], "find") == 0)
+            { // se siamo qui datiInformazioni[1] = cognome, datiInformazioni[2] = Num. persone, datiInformazioni[3] = data e datiInformazioni[4] = ora
+                // analizza la stringa e assegna i valori alle variabili
+                sscanf(datiInformazioni[3], "%d-%d-%d", &giorno, &mese, &anno);
+                sscanf(datiInformazioni[4], "%d", &ora);
+                sscanf(datiInformazioni[2], "%d", &numPersone);
+
+                codice = "find\0";
+                // controllo data inserita
+                if (data_valida(giorno, mese, anno, ora))
+                {
+                    if (data_futura(giorno, mese, anno, ora))
+                    {
+
+                        // mando codice "find"
+                        ret = send(sockfd, (void *)codice, codiceLen, 0);
+                        gestione_errore(ret, sockfd);
+
+                        sscanf(datiInformazioni[1], "%s", cognome);
+
+                        sprintf(buffer, "%d-%d-%d-%d %d %s", numPersone, giorno, mese, anno, ora, cognome);
+                        len_HO = strlen(buffer) + 1;
+                        len_NO = htonl(len_HO);
+
+                        // mando lunghezza del messaggio
+                        ret = send(sockfd, &len_NO, sizeof(uint32_t), 0);
+                        gestione_errore(ret, sockfd);
+
+                        // mando il vero e proprio MESSAGGIO
+                        ret = send(sockfd, (void *)buffer, len_HO, 0);
+                        gestione_errore(ret, sockfd);
+
+                        sleep(1);
+
+                        printf("I tavoli disponibili che soddisfano la tua richiesta sono:\n");
+                        tavoliDisp = 0;
+                        for (;;)
+                        {
+                            ret = recv(sockfd, &len_NO, sizeof(uint32_t), 0);
+                            gestione_errore(ret, sockfd);
+                            len_HO = ntohl(len_NO);
+
+                            ret = recv(sockfd, buffer, len_HO, 0);
+                            gestione_errore(ret, sockfd);
+
+                            if (strncmp(buffer, "STOP", strlen("STOP")) == 0) // per fare terminare il loop
+                            {
+                                printf("\n");
+                                fflush(stdout);
+                                if (tavoliDisp == 0)
+                                { // se nessun tavolo è stato proposto, avviso
+                                    printf("Non sono disponibili tavoli che soddisfino i requisiti richiesti. \nTi invitiamo a provare con una data differente o con un numero inferiore di ospiti.\n");
+                                    fflush(stdout);
+                                }
+                                break;
+                            }
+                            tavoliDisp++;
+                            printf("%s\n", buffer);
+                        }
+                        fflush(stdout);
+
+                        // TAVOLI DISPONIBILI
+                        ordine = 1; // adesso è possibile selezionare book
+                    }
+                    else
+                    {
+                        printf("La data inserita non risulta essere futura rispetto all'attuale momento corrente.\n");
+                        continue;
+                    }
+                }
+                else
+                {
+                    printf("La data inserita non è valida.\n");
+                    continue;
+                }
+
+                // dopo questo possiamo andare a book. Inserito alla fine, dopo tutte le conferme è valido
+            }
+            else if (strcmp(datiInformazioni[0], "book") == 0 && ordine == 0)
+            {
+                printf("Prima del book eseguire una Find correttamente\n\n");
+                fflush(stdout);
+            }
+            else if (strcmp(datiInformazioni[0], "book") == 0 && ordine == 1)
+            {
+
+                sscanf(datiInformazioni[1], "%d", &quantita);
+
+                if (tavoliDisp < quantita) // se il numeri scelto è > di quanti la find abbia restituito
+                {
+                    printf("Numero non valido, ri eseguire la prenotazione...\n");
+                    // ordine = 0;
+                    fflush(stdout);
+                    continue;
+                }
+                else
+                {
+                    // mando codice "book"
+                    codice = "book\0";
+                    ret = send(sockfd, (void *)codice, codiceLen, 0);
+                    gestione_errore(ret, sockfd);
+
+                    sprintf(buffer, "%d %d-%d-%d-%d %d %s", quantita, numPersone, giorno, mese, anno, ora, cognome);
+                    len_HO = strlen(buffer) + 1;
+                    len_NO = htonl(len_HO);
+
+                    ret = send(sockfd, &len_NO, sizeof(uint32_t), 0);
+                    gestione_errore(ret, sockfd);
+
+                    // mando tutti i dati per la prenotazione
+                    ret = send(sockfd, (void *)buffer, len_HO, 0);
+                    gestione_errore(ret, sockfd);
+
+                    // ricevo se ha confermato prenotazione o no
+                    ret = recv(sockfd, &len_NO, sizeof(uint32_t), 0);
+                    gestione_errore(ret, sockfd);
+                    len_HO = ntohl(len_NO);
+
+                    ret = recv(sockfd, buffer, len_HO, 0);
+                    gestione_errore(ret, sockfd);
+
+                    if (strcmp(buffer, "NO") == 0)
+                    {
+                        printf("Ripetere prenotazione. \n Il tavolo scelto è stato occupato. \n");
+                    }
+                    else
+                    {
+                        memset(buffer, 0, sizeof(buffer));
+                        // ricevo se ha confermato prenotazione o no
+                        ret = recv(sockfd, &len_NO, sizeof(uint32_t), 0);
+                        gestione_errore(ret, sockfd);
+                        len_HO = ntohl(len_NO);
+
+                        ret = recv(sockfd, buffer, len_HO, 0);
+                        gestione_errore(ret, sockfd);
+                        printf("PRENOTAZIONE EFFETTUATA, codice: %s.\n", buffer);
+                    }
+                }
+            }
+            else if (strcmp(datiInformazioni[0], "esc") == 0)
+            {
+                printf("Uscita in corso...\n\n");
+                fflush(stdout);
+                close(sockfd);
+                exit(0);
+            }
+            else
+            { // nessun comando inserito
+                printf("ERRORE! Comando inserito non valido. RIPROVARE...\n\n");
+                continue;
+            }
+        }
+    }
+
+    close(sockfd);
+    return 0;
+}

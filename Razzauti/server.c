a86658f01962c4b57a2399ad6cd6196aea5ee189
SimoneRazzauti <s.razzauti97@gmail.com>
2024-05-28 12:01:26 +0200

    up

--- Razzauti/server.c
+++ Razzauti/server.c
@@ -17,10 +17,10 @@
 #define WELCOME_SERVER "\n********************** AVVIO SERVER **********************\n"\
 "* \t\t\t\tDigita un comando: \t\t\t\t\t*\n"\
 "* \t\t\t\t\t\t\t\t\t\t\t*\n"\
-"* \tstat --> mostra lo stato delle comande ai vari tavoli\t*\n"\
-"* \tstop --> termina il server\t\t\t\t\t\t*\n"\
+"* stat --> mostra lo stato delle comande ai vari tavoli\t*\n"\
+"* stop --> termina il server\t\t\t\t*\n"\
 "* \t\t\t\t\t\t\t\t\t\t\t*\n"\
-"*********************************************************\n"
+"**********************************************************\n"
 
 #define MAX_PIATTI 10 // numero massimo di piatti nel menu
 #define MAX_KITCHENDEVICES 10 // numero massio di kitchen device connessi

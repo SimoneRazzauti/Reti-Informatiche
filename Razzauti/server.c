2b83f6329aa06a8f4775420fca2cf2b7b2116906
SimoneRazzauti <s.razzauti97@gmail.com>
2024-05-28 12:06:35 +0200

    up

--- Razzauti/server.c
+++ Razzauti/server.c
@@ -15,11 +15,11 @@
 #define MAX_TAVOLI 15 // numero massimo di tavoli nel ristorante
 #define BUFFER_SIZE 1024
 #define WELCOME_SERVER "\n*********************** AVVIO SERVER ***********************\n"\
-"* \t\t\t\tDigita un comando: \t\t\t\t\t*\n"\
-"* \t\t\t\t\t\t\t\t\t\t\t*\n"\
-"*  stat --> mostra lo stato delle comande ai vari tavoli \t*\n"\
-"*  stop --> termina il server \t\t\t\t*\n"\
-"* \t\t\t\t\t\t\t\t\t\t\t*\n"\
+"* \t\t\t\t\tDigita un comando: \t\t\t\t\t*\n"\
+"* \t\t\t\t\t\t\t\t\t\t\t\t\t\t*\n"\
+"*  stat --> mostra lo stato delle comande ai vari tavoli \t\t\t*\n"\
+"*  stop --> termina il server \t\t\t\t\t\t\t*\n"\
+"* \t\t\t\t\t\t\t\t\t\t\t\t\t\t*\n"\
 "************************************************************\n"
 
 #define MAX_PIATTI 10 // numero massimo di piatti nel menu
